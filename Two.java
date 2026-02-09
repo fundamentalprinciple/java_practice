5624ae6ff11affd73c7fdc2f22059d01ceaf39c7
fundamentalprinciple <princedixit931@gmail.com>
2026-02-09 12:48:21 +0000

    fixed the node class

--- Two.java
+++ Two.java
@@ -1,12 +1,21 @@
 public class Two {
     public static void main(String[] args) {
+
         class Node {
             int value;
             Node next;
+
+            Node(int value, Node next) {
+                this.value = value;
+                this.next = next;
+            }
         }
-        Node head = Node(5, null);
-        Node tail = Node(10, null);
+
+        Node head = new Node(5, null);
+        Node tail = new Node(10, null);
+        
         head.next = tail;
+        
         System.out.println(head.next.value);
     }
 }
